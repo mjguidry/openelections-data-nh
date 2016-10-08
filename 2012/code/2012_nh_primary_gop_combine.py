f1b32bdca7583a44906459339c48b2a5e41df308
Michael Guidry <m_guidry@msn.com>
2016-10-08 09:07:09 -0400

    Republican primary results

---
+++ 2012/code/2012_nh_primary_gop_combine.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 17:28:03 2016
+
+@author: mike
+"""
+
+
+rep_dir='../'
+csvfiles=['20120911__nh__republican__primary__executive__council__town.csv',
+          '20120911__nh__republican__primary__governor__town.csv',
+          '20120911__nh__republican__primary__house__1__town.csv',
+          '20120911__nh__republican__primary__house__2__town.csv',
+          '20120911__nh__republican__primary__state__house__town.csv',
+          '20120911__nh__republican__primary__state__senate__town.csv',
+          ]
+
+outfile=rep_dir+'20120911__nh__republican__primary__town.csv'
+o=open(outfile,'wb')
+
+for k,f_name in enumerate(csvfiles):
+    f=open(rep_dir+f_name,'rb')
+    if(k==0):
+        start_line=0
+    else:
+        start_line=1
+    for l,line in enumerate(f):
+        if(l>=start_line):         
+            o.write(line)
+    f.close()
+
+o.close()
